eb5560f84416492116112e2b96cf6a02c11edb94
alexander.brokle <alexander.brokle@biologis.com>
2019-07-21 12:57:40 +0200

    created empty list page and detail page, also added the routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
+import ListView from './Products/List';
+import DetailView from './Products/Detail';
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
       <div className="App">
-       Hello World
+        <Switch>
+          <Route exact path="/products" component={ListView} />
+          <Route path="/products/:id" component={DetailView} />
+          <Route render={() => <Redirect to="/products" />} />
+        </Switch>
       </div>
-   
+    </BrowserRouter>
     
   );
 }
